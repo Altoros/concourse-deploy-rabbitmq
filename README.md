42f5263a084f095ecde75fe21a55f06748abe440
A.S. Lomoff <lomov.as@gmail.com>
2017-02-15 17:46:37 -0800

    add workaround with setup-pipeline.sh

--- README.md
+++ README.md
@@ -7,83 +7,14 @@ Deploy RabbitMQ with [omg](https://github.com/enaml-ops) in a Concourse pipeline
 1. [Git](https://git-scm.com)
 1. [Vault](https://www.vaultproject.io)
 1. [Concourse](http://concourse.ci)
+1. [direnv](http://direnv.net)
 
 ## Steps to use this pipeline
 
-1. Clone this repository.
-
-    ```
-    git clone https://github.com/enaml-ops/concourse-deploy-rabbitmq.git
-    ```
-
-1. Copy the sample config file `deployment-props-sample.json`.
-
-    ```
-    cd concourse-deploy-rabbitmq
-    cp deployment-props-sample.json deployment-props.json
-    ```
-
-1. Edit `deployment-props.json`, adding the appropriate values.
-
-    This file is used to populate a `vault` hash.  It holds the BOSH credentials for both `omg` (username/password) and the Concourse `bosh-deployment` (UAA client) resource.
-
-    ```
-    $EDITOR deployment-props.json
-    ```
-
-    `omg` will also read other key/value pairs added here, matching them to command-line arguments.  For example, to add the `omg` plugin parameter `--syslog-address`, you could add `"syslog-address": "10.150.12.10"` here rather than modifying the manifest generation script in `ci/tasks`.
-
-    All available parameters/keys can be listed by querying the plugin.  If not specified in `deployment-props.json`, default values will be used where possible.
-
-    ```
-    omg-linux deploy-product p-rabbitmq-plugin-linux --help
-    ```
-
-1. Load your deployment properties into `vault`.  `VAULT_HASH` you define here and `vault_hash_misc` in `pipeline-vars.yml` below must match.  You may consider using the `vault` hash here to hold common settings, referenced by multiple `omg`-based deployments.  In such a case, you might name the hash something like `secret/nonprod-common-props`.
-
-    ```
-    export VAULT_ADDR=http://YOUR_VAULT_ADDR:8200
-    export VAULT_HASH=secret/rabbitmq-nonprod-props
-    vault write $VAULT_HASH @deployment-props.json
-    ```
-
-1. Delete or move `deployment-props.json` to a secure location.
-1. Copy the pipeline variables template.
-
-    ```
-    cp pipeline-vars-template.yml pipeline-vars.yml
-    ```
-
-1. Edit `pipeline-vars.yml`, adding appropriate values.
-
-    ```
-    $EDITOR pipeline-vars.yml
-    ```
-
-    Note: When you are deploying Pivotal RabbitMQ, you must add your `API Token` found at the bottom of your [Pivotal Profile](https://network.pivotal.io/users/dashboard/edit-profile) page.
-
-1. Create or update the pipeline.
-
-    ```
-    fly -t TARGET set-pipeline -p deploy-rabbitmq -c ci/pivotal-rabbitmq-service-pipeline.yml -l pipeline-vars.yml
-    ```
-
-    _or_
-
-    ```
-    fly -t TARGET set-pipeline -p deploy-rabbitmq -c ci/pivotal-rabbitmq-service-pipeline.yml -l pipeline-vars.yml
-    ```
-
-1. Delete or move `pipeline-vars.yml` to a secure location.
-1. Unpause the pipeline
-
-    ```
-    fly -t TARGET unpause-pipeline -p deploy-rabbitmq
-    ```
-
-1. Trigger the deployment job and observe the output.
-
-    ```
-    fly -t TARGET trigger-job -j deploy-rabbitmq/get-product-version -w
-    fly -t TARGET trigger-job -j deploy-rabbitmq/deploy -w
-    ```
+```
+git clone https://github.com/Altoros/concourse-deploy-rabbitmq.git
+cp .envrc{.example,}
+vi .envrc
+direnv allow
+./setup-pipeline.sh
+```
---
+++ setup-pipeline.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ -z $CONCOURSE_URI || -z $CONCOURSE_TARGET || -z $CONCOURSE_USER || -z $CONCOURSE_PASSWORD || \
+      -z $FOUNDATION_NAME || -z $PRODUCT_NAME || \
+      -z $BOSH_CLIENT || -z $BOSH_CLIENT_SECRET || -z $BOSH_CA_CERT || -z $BOSH_CA_CERT ]]; then
+  echo "one the following environment variables is not set: "
+  echo ""
+  echo "                 CONCOURSE_URI"
+  echo "                 CONCOURSE_TARGET"
+  echo "                 CONCOURSE_USER"
+  echo "                 CONCOURSE_PASSWORD"
+  echo "                 BOSH_CLIENT"
+  echo "                 BOSH_CLIENT_SECRET"
+  echo "                 BOSH_CA_CERT"
+  echo ""
+  exit 1
+fi
+
+export VAULT_HASH=secret/$PRODUCT_NAME-$FOUNDATION_NAME-props
+export CF_VAULT_PASSWORD_HASH=secret/cf-$FOUNDATION_NAME-password
+export CF_VAULT_PROPS_HASH=secret/cf-$FOUNDATION_NAME-props
+
+echo "requires files (rootCA.pem, director.pwd, deployment-props.json)"
+vault write ${VAULT_HASH} \
+  bosh-cacert=@$BOSH_CA_CERT \
+  bosh-pass=$BOSH_CLIENT_SECRET \
+  bosh-client-secret=$BOSH_CLIENT_SECRET \
+  bosh-client-id=director \
+  bosh-url=https://$BOSH_ENVIRONMENT \
+  bosh-port=25555 \
+  bosh-user=admin \
+  system-services-password=$(vault read --field=system-services-password $CF_VAULT_PASSWORD_HASH) \
+  doppler-zone=$(vault read --field=doppler-zone $CF_VAULT_PASSWORD_HASH) \
+  doppler-shared-secret=$(vault read --field=doppler-shared-secret $CF_VAULT_PASSWORD_HASH) \
+  nats-pass=$(vault read --field=nats-pass $CF_VAULT_PASSWORD_HASH) \
+  @deployment-props.json
+
+vault read --format=json $VAULT_HASH | jq .data > temp/vault-values.json
+
+j2y temp/vault-values.json > temp/vault-values.yml
+
+fly -t $CONCOURSE_TARGET login -c $CONCOURSE_URI -u $CONCOURSE_USER -p $CONCOURSE_PASSWORD
+
+fly -t $CONCOURSE_TARGET set-pipeline -p $PRODUCT_NAME-$FOUNDATION_NAME \
+              --config="ci/pipeline.yml" \
+              --var="vault-address=$VAULT_ADDR" \
+              --var="vault-token=$VAULT_TOKEN" \
+              --var="concourse-url=$CONCOURSE_URI" \
+              --var="concourse-user=$CONCOURSE_USER" \
+              --var="concourse-pass=$CONCOURSE_PASSWORD" \
+              --var="deployment-name=$PRODUCT_NAME-$FOUNDATION_NAME" \
+              --var="vault_addr=$VAULT_ADDR" \
+              --var="vault_token=$VAULT_TOKEN" \
+              --var="foundation-name=$FOUNDATION_NAME" \
+              --var="pipeline-repo=$PIPELINE_REPO" \
+              --var="pipeline-repo-branch=$PIPELINE_REPO_BRANCH" \
+              --var="pipeline-repo-private-key=$PIPELINE_REPO_PRIVATE_KEY_PATH" \
+              --var="product-name=$PRODUCT_NAME" \
+              --var="vault_hash_hostvars=secret/$PRODUCT_NAME-$FOUNDATION_NAME-hostvars" \
+              --var="vault_hash_ip=secret/$PRODUCT_NAME-$FOUNDATION_NAME-props" \
+              --var="vault_hash_keycert=secret/$PRODUCT_NAME-$FOUNDATION_NAME-keycert" \
+              --var="vault_hash_misc=secret/$PRODUCT_NAME-$FOUNDATION_NAME-props" \
+              --var="vault_hash_password=secret/$PRODUCT_NAME-$FOUNDATION_NAME-password" \
+              --var="vault_hash_ert_password=secret/cf-$FOUNDATION_NAME-password" \
+              --var="vault_hash_ert_ip=$CF_VAULT_PROPS_HASH" \
+              --load-vars-from pipeline-defaults.yml \
+              --load-vars-from temp/vault-values.yml 
+
+fly -t $CONCOURSE_TARGET unpause-pipeline -p $PRODUCT_NAME-$FOUNDATION_NAME
